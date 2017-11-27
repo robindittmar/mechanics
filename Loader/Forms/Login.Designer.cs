32a8381f034d79ca47af54609aa72daccc191c6d
Nico Piechaczek <nico.piechaczek@online.de>
2017-11-27 21:39:36 +0100

    Loader added

---
+++ Loader/Forms/Login.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Loader
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtPw = new System.Windows.Forms.TextBox();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblPw = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(15, 72);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(180, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(50, 17);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(145, 20);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtPw
+            // 
+            this.txtPw.Location = new System.Drawing.Point(50, 46);
+            this.txtPw.Name = "txtPw";
+            this.txtPw.Size = new System.Drawing.Size(145, 20);
+            this.txtPw.TabIndex = 1;
+            this.txtPw.UseSystemPasswordChar = true;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(12, 20);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(21, 13);
+            this.lblId.TabIndex = 3;
+            this.lblId.Text = "ID:";
+            // 
+            // lblPw
+            // 
+            this.lblPw.AutoSize = true;
+            this.lblPw.Location = new System.Drawing.Point(12, 49);
+            this.lblPw.Name = "lblPw";
+            this.lblPw.Size = new System.Drawing.Size(28, 13);
+            this.lblPw.TabIndex = 4;
+            this.lblPw.Text = "PW:";
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 108);
+            this.Controls.Add(this.lblPw);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtPw);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtPw;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblPw;
+    }
+}
---
+++ Loader/Forms/Main.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Loader
+{
+    enum HackStatus
+    {
+        Undetected,
+        Updating,
+        Unknown
+    }
+
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            // TODO: get player informations
+            lblUsername.Text = "Myridor";
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            // TODO: check if player is allowed to inject
+            // TODO: get informations about hack
+            HackStatus iHackStatus = (HackStatus)listBox.SelectedIndex;
+            bool bEnableInject = false;
+            DateTime dtExpiresOn = DateTime.Now;
+            dtExpiresOn = dtExpiresOn.AddMinutes(listBox.SelectedIndex);
+            // TODO:
+
+            lblExpiresOnValue.Text = dtExpiresOn.ToShortDateString() + ' ' + dtExpiresOn.ToShortTimeString();
+            lblExpiresOnValue.ForeColor = dtExpiresOn > DateTime.Now ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+
+            switch (iHackStatus)
+            {
+                case HackStatus.Unknown:
+                    lblStatusValue.Text = "Unknown";
+                    lblStatusValue.ForeColor = System.Drawing.Color.Orange;
+                    bEnableInject = true;
+                    break;
+                case HackStatus.Updating:
+                    lblStatusValue.Text = "Updating";
+                    lblStatusValue.ForeColor = System.Drawing.Color.Red;
+                    bEnableInject = false;
+                    break;
+                case HackStatus.Undetected:
+                    lblStatusValue.Text = "Undetected";
+                    lblStatusValue.ForeColor = System.Drawing.Color.Green;
+                    bEnableInject = true;
+                    break;
+            }
+
+            btnInject.Enabled = bEnableInject;
+        }
+
+        private void btnInject_Click(object sender, System.EventArgs e)
+        {
+            // TODO: inject the game
+        }
+    }
+}
